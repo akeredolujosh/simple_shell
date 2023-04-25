71e52a474e2406135220739c07ebd07a27efe18b
Tsitsi Ndhlovu <tsitsindhlovu211@gmail.com>
2023-04-25 10:24:32 -0700

    TsitsiNdhlovu_commit_6

---
+++ errors.c
@@ -0,0 +1,86 @@
+#include "shell.h"
+
+/**
+ * _eputs - prints string input
+ * @str: printed string
+ * Return: 0
+ */
+
+void _eputs(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_eputchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _eputchar - written stderr character c
+ * @c: printed character
+ * Return: 1 on success and -1 on error
+ * setting errno appropritely
+ */
+
+int _eputchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _putfd - writes character c to fd
+ * @c: printed character
+ * @fd: file description to write
+ * Return: 1 on success and -1 on error
+ * errno is set appropriately
+ */
+
+int _putfd(char c, int fd)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _putsfd - prints input string
+ * @str: printed string
+ * @fd: file descriptior to write into
+ * Return: the char put number
+ */
+
+int _putsfd(char *str, int fd)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		i += _putfd(*str++, fd);
+	}
+	return (i);
+}
+
